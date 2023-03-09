dc4788eed6b7d29c732418018b8a63afefbdce3d
MaksymKostielei <maxim.kosteley@gmail.com>
2023-03-09 15:35:07 +0200

    done task 2.1, 2.2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { getPadTime } from './helpers/getPadTime';
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [isCounting, setIsCounting] = useState(false);
+
+  const hours = getPadTime(Math.floor(time / 3600));
+  const minutes = getPadTime(Math.floor((time - (hours * 3600)) / 60));
+  const seconds = getPadTime(time % 60);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      isCounting && setTime((time) => time = time + 1);
+    }, 1000);
+    return () => {
+      clearInterval(interval)
+    }
+  }, [isCounting]);
+
+  const handleStart = () => {
+    setIsCounting(true);
+  };
+
+  const handleStop = () => {
+    setIsCounting(false);
+  };
+
+  const handleReset = () => {
+    setIsCounting(false);
+    setTime(0);
+  };
+  
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='timer'>
+        <span>{hours}</span>
+        <span>:</span>
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{seconds}</span>
+      </div>
+      <div className='buttons'>
+        <button onClick={handleStart}>Start</button>     
+        <button onClick={handleStop}>Stop</button>
+        <button onClick={handleReset}>Reset</button>
+      </div>
     </div>
+    
   );
+ 
 }
 
 export default App;
